b3adc46b051494014030b4959c9ef37448b436f5
Pathum74 <pathumprathibha74@gmail.com>
2024-08-30 20:21:53 +0530

    Practical 01 02

---
+++ 0102.c
@@ -0,0 +1,19 @@
+/*Display the following output using printf() statements
+*
+**
+***
+****
+*****
+*/
+
+#include <stdio.h> 
+ 
+int main() 
+{ 
+    printf("*\n"); 
+    printf("**\n"); 
+    printf("***\n"); 
+    printf("****\n"); 
+    printf("*****\n"); 
+return 0; 
+}
