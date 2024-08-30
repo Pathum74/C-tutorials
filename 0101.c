3c2824ea3848396003e398749677b47d8b24bbf9
Pathum74 <pathumprathibha74@gmail.com>
2024-08-30 19:58:57 +0530

    Practical 01 01

---
+++ 0101.c
@@ -0,0 +1,10 @@
+//Display your name and school name in two separate lines
+
+#include <stdio.h> 
+ 
+int main() 
+{ 
+    printf("Pathum\n"); 
+    printf("Rajasinghe college"); 
+    return 0; 
+}
